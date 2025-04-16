31554d0c7c8e99dc3e1efb3c68c11f1183dc4545
Anderson Campos Padilha <campospadilha.a@gmail.com>
2025-04-16 14:42:24 -0300

    Configurados: variáveis de ambiente e arquivo 'application.properties'. Criadas também as classes: 'ChatService' e 'GenerativIAControler'

---
+++ src/main/java/br/com/campospadilhaa/controller/GenerativeIAController.java
@@ -0,0 +1,23 @@
+package br.com.campospadilhaa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.campospadilhaa.service.ChatService;
+
+@RestController
+public class GenerativeIAController {
+
+	private final ChatService chatService;
+
+	public GenerativeIAController(ChatService chatService) {
+		this.chatService = chatService;
+	}
+
+	@GetMapping("ask-ai")
+	public String getResponse(@RequestParam String prompt) {
+
+		return chatService.getResponse(prompt);
+	}
+}
